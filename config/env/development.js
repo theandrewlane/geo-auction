18ba73e8978ccb54167f44f956e0bfff20a89469
theandrewlane <andrew.bryan12@gmail.com>
2016-06-21 23:38:01 -0600

    heroku stuff

--- config/env/development.js
+++ config/env/development.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var defaultEnvConfig = require('./default');
-
+console.log(db);
 module.exports = {
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     options: {
-      user: '',
-      pass: ''
+      user: 'admin',
+      pass: 'admin'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
